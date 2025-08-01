fa89526e79b480491c98085d8826904a50723b3c
sarana1203 <saranariharasuthan@gmail.com>
2025-08-02 00:38:10 +0530

    web.xml_fix

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -10,6 +10,6 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello Saranya! You did it!!</h1>");
+        response.getWriter().println("<h1>Hello Saranya!</h1>");
     }
 }
